61041e516ee4435e28601616a83db04bba13171f
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-01 16:15:42 -0600

    Added test suite to test for Booklist class

---
+++ test/com/twu/biblioteca/BookListTest.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BookListTest {
+    @Test
+    public void populateTest() {
+        assertEquals(1, 1);
+    }
+}
