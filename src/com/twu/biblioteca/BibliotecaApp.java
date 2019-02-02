b6c6cb75685a10d58bc62e0047e339e26594d550
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-01 21:25:38 -0600

    Made a visual empty table to be populated bu books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,5 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.*;
-import java.util.List;
 
 public class BibliotecaApp {
 
--- src/com/twu/biblioteca/TableMaker.java
+++ src/com/twu/biblioteca/TableMaker.java
@@ -1,4 +1,49 @@
 package com.twu.biblioteca;
 
 public class TableMaker {
+    private int rows;
+    private int columns;
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+
+    public TableMaker() {
+
+    }
+
+    public  String makeTable() {
+
+        String[][] table = new String[columns][rows];
+        String visualizeTable = "";
+
+        for(int horizontal = 0; horizontal < table.length;horizontal++) {
+            for(int vertical = 0; vertical < table[0].length;vertical++) {
+                visualizeTable += " | " + table[horizontal][vertical];
+            }
+            visualizeTable += " | \n";
+        }
+        return visualizeTable;
+    }
+
+    public static void  main(String[] args) {
+        System.out.println("The making of the table");
+        TableMaker showTable = new TableMaker();
+        showTable.setRows(3);
+        showTable.setColumns(8);
+        System.out.println(showTable.makeTable());
+    }
 }
