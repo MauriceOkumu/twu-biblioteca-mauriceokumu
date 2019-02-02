371b36e0eb40ed26fe2b58cba5747fe0a60fb5cf
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-02 01:54:45 -0600

    Tested menu for proper user input capture

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -5,35 +5,17 @@ import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
-//public class BookListTest {
-//    private BookList testBookList;
-//    @Before
-//    public void initializeClass() throws Exception {
-//        testBookList =  new BookList();
-//        testBookList.populate();
-//    }
-//    @Test
-//    public void populateTest() {
-//        assertEquals(testBookList.list.get(1).get(1), "The Heightsing");
-//    }
-//    @Test
-//    public void showListTest() {
-//        assertEquals(testBookList.showList().length(),205);
-//    }
-//    @Test
-//    public void checkList() {
-//        assertEquals(testBookList.viewList().length(), 75);
-//    }
-//}
+
 public class MenuTest {
-    private Menu menu;
+    private Menu menuTest;
     @Before
     public void createMenu() {
-        menu = new Menu();
+        menuTest = new Menu();
+        menuTest.menu.add("List of Books");
     }
 
     @Test
     public void testMenu() {
-        assertEquals(menu.size(), 1);
+        assertEquals(menuTest.menu.size(), 1);
     }
 }
