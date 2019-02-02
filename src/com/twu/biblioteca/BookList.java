700239dcea9caceb8385082ac7d76f1f9294f120
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-01 23:54:11 -0600

    Showing books by Author, Title and Year published

--- src/com/twu/biblioteca/BookList.java
+++ src/com/twu/biblioteca/BookList.java
@@ -2,30 +2,55 @@ package com.twu.biblioteca;
 
 import java.util.*;
 public class BookList {
-    public ArrayList list;
+    public List <List<String>> list;
 
     public BookList() {
-        this.list = new ArrayList<>();
+        this.list = new ArrayList<>(new ArrayList<>());
     }
 
     public void populate() {
-        list.add("Armageddon");
-        list.add("Counterfeiters");
-        list.add("The Nest");
+        String[] a = new String[]{"Tom Blazi", "The Heightsing", "1967"};
+        String[] b = new String[]{"Sidney Dr", "Luna Moon Diet", "1999"};
+        String[] c = new String[]{"Igwe Root", "Exotic Birdsle", "2011"};
+        String[] d = new String[]{"Alma Mase", "Beautiful Dogs", "2000"};
+        String[] e = new String[]{"AUTHOR---", "BOOK TITLE----", "YEAR"};
+
+        List<String> authorA = new ArrayList<>();
+        List<String> authorB = new ArrayList<>();
+        List<String> authorC = new ArrayList<>();
+        List<String> authorD = new ArrayList<>();
+        List<String> authorE = new ArrayList<>();
+
+        authorA.addAll(Arrays.asList(e));
+        authorB.addAll(Arrays.asList(a));
+        authorC.addAll(Arrays.asList(b));
+        authorD.addAll(Arrays.asList(c));
+        authorE.addAll(Arrays.asList(d));
+
+        list.add(authorA);
+        list.add(authorB);
+        list.add(authorC);
+        list.add(authorD);
+        list.add(authorE);
     }
 
-    public Boolean showList() {
-//        Iterator iterator = list.iterator();
-//        while(iterator.hasNext()) {
-//            System.out.println(iterator.next());
-//        }
-        int count = 0;
-        while (list.size() > count) {
-            System.out.println(count+1 +". "+list.get(count));
-            count++;
+    public String showList() {
 
+        String visualizeTable = "";
+        int counter = 0;
+
+        for(int horizontal = 0; horizontal < list.size();horizontal++) {
+            visualizeTable+=counter;
+            counter++;
+            for(int vertical = 0; vertical < list.get(0).size();vertical++) {
+                visualizeTable += " | " + list.get(horizontal).get(vertical);
+            }
+            visualizeTable += " | \n";
         }
-        return true;
+
+
+        return visualizeTable;
+
 
     }
 }
