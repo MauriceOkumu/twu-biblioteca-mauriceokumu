a4903ca54527be84e453a337885e3675b3851655
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-02 01:54:03 -0600

    Added user input option in menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,17 +1,15 @@
 package com.twu.biblioteca;
 
-
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
         BookList bookLists = new BookList();
-
+        Menu showOtions = new Menu();
+        showOtions.addToMenu("List of Books");
         System.out.println("Welcome to Biblioteca.Your one-stop shop for great books in Bangalore!");
         bookLists.populate();
-        System.out.println("These are some of the books in our collection.");
-        System.out.println(bookLists.viewList());
-        System.out.println(bookLists.showList());
+        showOtions.showMenu();
 
     }
 }
