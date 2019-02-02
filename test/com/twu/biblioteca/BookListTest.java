8af323d63bd120e4da009d8d8507dcda97593dd1
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-01 21:26:30 -0600

    Check to see that the table generator works properly

--- test/com/twu/biblioteca/BookListTest.java
+++ test/com/twu/biblioteca/BookListTest.java
@@ -3,7 +3,6 @@ package com.twu.biblioteca;
 import org.junit.Test;
 import org.junit.Before;
 
-import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
--- test/com/twu/biblioteca/TableMakerTest.java
+++ test/com/twu/biblioteca/TableMakerTest.java
@@ -1,4 +1,23 @@
 package com.twu.biblioteca;
 
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
 public class TableMakerTest {
+    private TableMaker tableMaker;
+
+    @Before
+    public void createTableTest() throws Exception {
+        tableMaker =  new TableMaker();
+        tableMaker.setRows(3);
+        tableMaker.setColumns(6);
+    }
+    @Test
+    public  void makeTableTest() {
+        assertEquals(tableMaker.getRows(), 3);
+        assertEquals(tableMaker.getColumns(), 6);
+    }
 }
