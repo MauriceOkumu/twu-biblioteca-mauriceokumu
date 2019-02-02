c0a9eb0d79e8ce9f0c946080c24242f9fe40aed8
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-02 01:52:58 -0600

    Add options for sowing list of books

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,4 +1,32 @@
 package com.twu.biblioteca;
 
+import java.util.*;
+import java.math.*;
 public class Menu {
+    public List menu;
+    public BookList bookLists ;
+
+    public Menu() {
+        this.menu = new ArrayList<>();
+    }
+
+    public void addToMenu(String item) {
+        menu.add(item);
+    }
+
+    public void showMenu() {
+        bookLists = new BookList();
+        bookLists.populate();
+
+        System.out.println("___MENU___");
+        System.out.println("1."+ menu.get(0)+"\n");
+        Scanner inputs = new Scanner(System.in);
+        System.out.print("Select 1 to look at our book collection ");
+
+        if(inputs.nextLine() .equals("1")) {
+            System.out.println(bookLists.showList()+"\n");
+        }
+        inputs.close();
+
+    }
 }
