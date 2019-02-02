b59f724f8c12959201d215eb6cf269fcc77a5eaa
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-01 19:05:00 -0600

    Added functionality to show all books

--- src/com/twu/biblioteca/BookList.java
+++ src/com/twu/biblioteca/BookList.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class BookList {
     public ArrayList list;
 
-    public  BookList() {
+    public BookList() {
         this.list = new ArrayList<>();
     }
 
@@ -14,4 +14,18 @@ public class BookList {
         list.add("The Nest");
     }
 
+    public Boolean showList() {
+//        Iterator iterator = list.iterator();
+//        while(iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+        int count = 0;
+        while (list.size() > count) {
+            System.out.println(count+1 +". "+list.get(count));
+            count++;
+
+        }
+        return true;
+
+    }
 }
