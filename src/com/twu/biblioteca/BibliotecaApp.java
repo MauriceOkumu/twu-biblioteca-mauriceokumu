4e034f27bd398c2fb7451722d0b562fa390634cf
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-01 18:01:50 -0600

    Initialized new booklist without arguments

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,11 +8,11 @@ import java.util.List;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        ArrayList<String> listOfBooks = new ArrayList<>();
-        BookList bookLists = new BookList(listOfBooks);
+
+        BookList bookLists = new BookList();
 
         System.out.println("Welcome to Biblioteca.Your one-stop shop for great books in Bangalore!");
         bookLists.populate();
-        System.out.println("Booklist is populated "+ bookLists.list.get(1));
+        System.out.println("Booklist is populated ");
     }
 }
