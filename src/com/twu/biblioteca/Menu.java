28273703cbe5eb235ecbf15539f6bd26a8ff8c7f
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-02 23:32:32 -0600

    Added Quiting from application option

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -4,39 +4,98 @@ import java.util.*;
 import java.math.*;
 public class Menu {
     public List menu;
+    public BookCheckout allBooks;
     public BookList bookLists ;
-    public String userInput = "1",input;
+    public String userInput = "1",input,checkout;
+    public Reader reader;
+    public String optOut;
 
     public Menu() {
         this.menu = new ArrayList<>();
     }
 
-    public void addToMenu(String item) {
-        menu.add(item);
+    public void addToMenu() {
+        menu.add("List of Books");
     }
 
     public void showMenu() {
-        bookLists = new BookList();
-        bookLists.populate();
+//        allBooks = new BookCheckout();
+//        allBooks.startProcess();
+//        bookLists = allBooks.bookLists;
 
         System.out.println("___MENU___");
         System.out.println("1."+ menu.get(0)+"\n");
-        Scanner inputs = new Scanner(System.in);
+        reader = new Reader();
+        setMenu("");
+    }
+
+    public void setMenu(String msg) {
 
 
         do{
-            String msg = "Please Select an option from the above menu!";
+             msg = "Please Select 1 from the menu above to see the list of books available! or quit to exit";
             System.out.print( msg );
-            input = inputs.nextLine();
+            input = reader.scan();
             if(userInput.equals(input)) {
                 System.out.println(bookLists.showList()+"\n");
+            } else if(!input.equals(userInput) && !input.equals("yes")){
+                System.out.println("Please select a valid option! Yes or 1");
             } else {
-                System.out.println("Please select a valid option!");
+                optToQuit();
             }
         }
         while(!input.equals(userInput));
 
-        inputs.close();
 
     }
+
+    public void checkOUt () {
+        System.out.print("Choose a number from above menu to checkout a book");
+        checkout = reader.scan();
+        int ind = Integer.parseInt(checkout);
+        //System.out.println("This is a check  =>"+ bookLists.list.get(ind));
+        //for( int i = 0; i < bookLists.list.size(); i++) {
+            if(bookLists.list.size() > ind) {
+                System.out.println("You successfully checked out a book by the title :"+ bookLists.list.get(ind).get(0));
+                allBooks.showMessage();
+                bookLists.list.remove(Integer.parseInt(checkout));
+                System.out.println("These are the remaining books!");
+                showMenu();
+
+            } else {
+
+              System.out.println("Sorry, that book is not available");
+              System.out.println("Checkout the list again for available books");
+              showMenu();
+              checkOUt();
+
+            }
+        //}
+        optToQuit();
+
+    }
+
+    public void optToQuit() {
+        System.out.print("Do you wish to quit the Application? yes to quit or no to continue");
+        optOut = reader.scan();
+        if(optOut.equals("yes")) {
+            System.exit(0);
+        } else {
+            System.out.println("Okay Here are the books we still have");
+            showMenu();
+        }
+
+    }
+
+    public void execute() {
+        addToMenu();
+        allBooks = new BookCheckout();
+        allBooks.startProcess();
+        bookLists = allBooks.bookLists;
+        showMenu();
+        checkOUt();
+
+    }
+
+
 }
