8dc87dd4fe7c9cc14e1df77f181c90bf115ee3c9
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-02 23:21:35 -0600

    Added Scanner class to reuse it

--- src/com/twu/biblioteca/Reader.java
+++ src/com/twu/biblioteca/Reader.java
@@ -1,4 +1,24 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
 public class Reader {
+    Scanner s ;
+
+    public Reader() {
+        s = new Scanner(System.in);
+    }
+
+    public String scan() {
+        String input;
+        if (s.hasNextLine()) {
+            input = s.nextLine();
+        } else {
+            input = "ERROR";
+        }
+        return input.toLowerCase();
+    }
+
+    public void close(){
+        s.close();
+    }
 }
