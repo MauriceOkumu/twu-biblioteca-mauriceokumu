8e72ae0b5fca6f055fc2cf2ea0f129e2de919bcf
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2019-02-02 23:23:43 -0600

    Added success message on checkout

--- src/com/twu/biblioteca/BookCheckout.java
+++ src/com/twu/biblioteca/BookCheckout.java
@@ -1,4 +1,35 @@
 package com.twu.biblioteca;
 
+
+
 public class BookCheckout {
+    private String preparedBook;
+    public BookList bookLists;
+
+
+    public void setPreparedBook(String preparedBook) {
+        this.preparedBook = preparedBook;
+    }
+
+    public String getPreparedBook() {
+        return preparedBook;
+    }
+
+
+    //@Override
+    public void returnBook(BookList lib, String book) {
+
+    }
+
+    //@Override
+    public void showMessage() {
+       System.out.println("Thank you Enjoy the Book!");
+    }
+
+    public void startProcess() {
+        bookLists = new BookList();
+        bookLists.populate();
+    }
+
+
 }
